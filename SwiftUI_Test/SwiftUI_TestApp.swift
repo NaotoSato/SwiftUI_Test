e425f35e477baa745d5d409a0b38e06d121918a7
佐藤 直人 <sato.naoto@furyu.jp>
2025-03-31 16:24:06 +0900

    関連ファイル群

---
+++ SwiftUI_Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI_Test
+//
+//  Created by 佐藤 直人 on 2025/03/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
