e87859474be9535dac54168285861b7973787eba
佐藤 直人 <sato.naoto@furyu.jp>
2025-04-11 18:52:44 +0900

    詳細画面のViewを分割＆Previewを用意

--- SwiftUI_Test/PokemonDetailView.swift
+++ SwiftUI_Test/PokemonDetailView.swift
@@ -11,85 +11,140 @@ import SDWebImageSwiftUI
 struct PokemonDetailView: View {
     let pokemonDetailDto: PokemonDetailDto
     
-    var body: some View {
+    // カードの外側の枠
+    var outsideFrame: some View {
+        Rectangle()
+            .foregroundColor(.originalGray)
+            .frame(width: 340, height: 590) // 高さを調整して2つの四角を統合
+            .border(Color.black, width: 1)
+            .cornerRadius(10)
+            .shadow(radius: 3, x: 10, y: 10)
+    }
+    
+    // カードの内側の枠
+    var insideFrame: some View {
+        Rectangle()
+            .foregroundColor(pokemonDetailDto.types.first?.color ?? .white)
+            .frame(width: 320, height: 570) // 高さを調整して2つの四角を統合
+            .border(Color.black, width: 1)
+            .cornerRadius(10)
+    }
+    
+    // ポケモン画像
+    var pokemonImage: some View {
+        VStack {
+            if let gifUrl = pokemonDetailDto.sprites.versions.generationV.blackWhite.animated.front_default, let url = URL(string: gifUrl) {
+                WebImage(url: url) // SDWebImageSwiftUI を使用
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400)
+            } else if let imageUrl = pokemonDetailDto.sprites.front_default, let url = URL(string: imageUrl) {
+                // gifが取得できなければ静止画を表示
+                AsyncImage(url: url) { image in
+                    image.resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+        }
+    }
+    
+    // ポケモン情報
+    var pokemonInfo: some View {
         ZStack {
-            // 大外の枠
             Rectangle()
-                .foregroundColor(.originalGray)
-                .frame(width: 340, height: 590) // 高さを調整して2つの四角を統合
-                .border(Color.black, width: 1)
-                .cornerRadius(10)
-                .shadow(radius: 3, x: 10, y: 10)
-            
-            // 中の枠
-            Rectangle()
-                .foregroundColor(pokemonDetailDto.types.first?.color ?? .white)
-                .frame(width: 320, height: 570) // 高さを調整して2つの四角を統合
+                .foregroundColor(.white)
+                .frame(width: 320, height: 160)
                 .border(Color.black, width: 1)
                 .cornerRadius(10)
             
             VStack {
-                // ポケモン画像
-                if let gifUrl = pokemonDetailDto.sprites.versions.generationV.blackWhite.animated.front_default, let url = URL(string: gifUrl) {
-                    WebImage(url: url) // SDWebImageSwiftUI を使用
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 400, height: 400)
-                } else if let imageUrl = pokemonDetailDto.sprites.front_default, let url = URL(string: imageUrl) {
-                    // gifが取得できなければ静止画を表示
-                    AsyncImage(url: url) { image in
-                        image.resizable()
-                            .scaledToFit()
-                            .frame(width: 400, height: 400)
-                    } placeholder: {
-                        ProgressView()
+                HStack {
+                    // 名前
+                    Text(pokemonDetailDto.name)
+                        .font(.headline)
+                        .padding()
+                    Spacer()
+                    // タイプ
+                    ForEach(pokemonDetailDto.types, id: \.self) { _type in
+                        Text(_type.name)
+                            .frame(width: 80, height: 20)
+                            .background(_type.color, in: RoundedRectangle(cornerRadius: 8))
                     }
                 }
+                .frame(width: 315, height: 20)
                 
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(.white)
-                        .frame(width: 320, height: 160)
-                        .border(Color.black, width: 1)
-                        .cornerRadius(10)
-                    
-                    VStack {
-                        HStack {
-                            // 名前
-                            Text(pokemonDetailDto.name)
-                                .font(.headline)
-                                .padding()
-                            Spacer()
-                            // タイプ
-                            ForEach(pokemonDetailDto.types, id: \.self) { _type in
-                                Text(_type.name)
-                                    .frame(width: 80, height: 20)
-                                    .background(_type.color, in: RoundedRectangle(cornerRadius: 8))
-                            }
-                        }
-                        .frame(width: 315, height: 20)
-                        
-                        // 分類
-                        Text(pokemonDetailDto.genus)
-                        
-                        HStack {
-                            // たかさ、おもさ
-                            let height = Double(pokemonDetailDto.height) * 0.1 // メートルにするため0.1かける
-                            let weight = Double(pokemonDetailDto.weight) * 0.1 // キログラムにするため0.1かける
-                            Text("たかさ：\(String(format: "%.1f", height))m") // 小数第一位まで表示
-                            Text("おもさ：\(String(format: "%.1f", weight))kg") // 小数第一位まで表示
-                        }
-                        .frame(width: 315, height: 15)
-                        
-                        Divider()
-                            .frame(width: 320)
-                        // 説明
-                        Text(pokemonDetailDto.flavorText)
-                    }
+                // 分類
+                Text(pokemonDetailDto.genus)
+                
+                HStack {
+                    // たかさ、おもさ
+                    let height = Double(pokemonDetailDto.height) * 0.1 // メートルにするため0.1かける
+                    let weight = Double(pokemonDetailDto.weight) * 0.1 // キログラムにするため0.1かける
+                    Text("たかさ：\(String(format: "%.1f", height))m") // 小数第一位まで表示
+                    Text("おもさ：\(String(format: "%.1f", weight))kg") // 小数第一位まで表示
                 }
-
+                .frame(width: 315, height: 15)
+                
+                Divider()
+                    .frame(width: 320)
+                // 説明
+                Text(pokemonDetailDto.flavorText)
+            }
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            // カードの外側の枠
+            outsideFrame
+            
+            // カードの内側の枠
+            insideFrame
+            
+            
+            VStack {
+                // ポケモン画像
+                pokemonImage
+                
+                // ポケモン情報
+                pokemonInfo
             }
         }
         .navigationTitle("詳細情報")
     }
+}
+
+#Preview {
+    let dto = PokemonDetailDto(
+        id: 1,
+        name: "リザードン",
+        height: 1,
+        weight: 1,
+        sprites: Sprites.init(
+            front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+            other: OtherSprites.init(
+                showdown: ShowdownSprites.init(
+                    front_default: ""
+                )
+            ),
+            versions: VersionSprites.init(
+                generationV: GenerationVSprites.init(
+                    blackWhite: BlackWhiteSprites.init(
+                        animated: AnimatedSprites.init(
+                            front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif"
+                        )
+                    )
+                )
+            )
+        ),
+        types: [.fire],
+        genus: "かえんポケモン",
+        flavorText: "あああああああああああああああ\nああああああああああああああ\nああああ")
+    
+    PokemonDetailView(
+        pokemonDetailDto: dto
+    )
 }
